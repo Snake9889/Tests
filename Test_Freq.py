e84dfdd2aacbf0c3fae968be06cc9de1e13684c0
Snake9889 <vya4eslaw.denisov@yandex.ru>
2022-01-05 15:44:15 +0700

    New test of ICA.

--- Test_Freq.py
+++ Test_Freq.py
@@ -4,24 +4,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import (linspace, sin, cos, pi)
 from numpy.fft import rfft, rfftfreq
+from sklearn.decomposition import FastICA
 
 # ===================================================================================
 
-x=linspace(0, 1000, 1000) #возвращает равномерно распределённые числа в данном интервале
+x = linspace(0, 1000, 1000)  # возвращает равномерно распределённые числа в данном интервале
 
-a0= 1
-a1= 0.5
-ω0= 0.017
-ω1= 0.34
+a0 = 1
+a1 = 0.5
+a2 = 1.5
+w0 = 0.017
+w1 = 0.072
+w2 = 0.002
+
 
 data_1 = (np.random.random(1000))
-noise = [10*x for x in data_1] #шум
+# noise = [1*x for x in data_1]  # шум
 
-#T= 2*pi
-f1 = a0*sin(2 * pi * ω0 * x + 2 * pi * 0.00) + [10*x for x in np.random.random(1000)]
-f2 = a0*sin(2 * pi * ω0 * x + 2 * pi * 0.00425) + [10*x for x in np.random.random(1000)] 
-f3 = a0*sin(2 * pi * ω0 * x + 2 * pi * 0.0085) + [10*x for x in np.random.random(1000)]
-f4 = a0*sin(2 * pi * ω0 * x + 2 * pi  * 0.01275) + [10*x for x in np.random.random(1000)] 
+# T= 2*pi
+f1 = a0*sin(2 * pi * w0 * x + 2 * pi * 0.00) + a1*sin(2 * pi * w1 * x + 2 * pi * 0.00) + a2*cos(2 * pi * w2 * x + 2 * pi * 0.00) + [1*x for x in np.random.random(1000)]
+f2 = a0*sin(2 * pi * w0 * x + 2 * pi * 0.00425) + a1*sin(2 * pi * w1 * x + 2 * pi * 0.00425) + a2*cos(2 * pi * w2 * x + 2 * pi * 0.00425) + [x for x in np.random.random(1000)]
+f3 = a0*sin(2 * pi * w0 * x + 2 * pi * 0.0085) + a1*sin(2 * pi * w1 * x + 2 * pi * 0.0085) + a2*cos(2 * pi * w2 * x + 2 * pi * 0.0085) + [x for x in np.random.random(1000)]
+f4 = a0*sin(2 * pi * w0 * x + 2 * pi * 0.01275) + a1*sin(2 * pi * w1 * x + 2 * pi * 0.01275) + a2*cos(2 * pi * w2 * x + 2 * pi * 0.01275) + [x for x in np.random.random(1000)]
 
 nrows, ncols = 4, 1
 figsize = [10, 7]
@@ -49,9 +53,10 @@ for i in range(len(x)):
     newMass[4*i + 2] = f3[i]
     newMass[4*i + 3] = f4[i]
 # ===================================================================================
-spectrf = rfft(newMass - np.mean(newMass)) / (len(x))
+
+spectrf = rfft(Mas - np.mean(Mas)) / (len(x))
 #nu = linspace(0, 2, 2001)
-nu = rfftfreq(4000, 1./4)
+nu = rfftfreq(1000, 1.)
 
 plt.figure(figsize=(8, 5))
 plt.plot(nu, np.abs(spectrf)) #возвращает частоту в Гц
